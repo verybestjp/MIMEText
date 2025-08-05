47c7b13f2e9b4e19bac0bb639925826745df84ac
aminov-kailash <aminov@kailash-tech.jp>
2025-08-05 12:56:24 +0900

    nodeだけ残す

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-    tabWidth: 4,
-    htmlWhitespaceSensitivity: 'ignore',
-    bracketSameLine: true,
-    plugins: [],
-}
-
-export default config
